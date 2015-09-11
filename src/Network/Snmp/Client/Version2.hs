ef947f424af210d5308d73abcd70ed43ca0e239b
Anton Kondrachkov <simplex91@gmail.com>
2015-09-11 18:27:47 +0300

    add requestAndFilterRid function, code cleanup
    receiveAndFilterRid receives packets and drops them till required rid is
    found

--- src/Network/Snmp/Client/Version2.hs
+++ src/Network/Snmp/Client/Version2.hs
@@ -21,39 +21,43 @@ import Network.Protocol.Snmp hiding (rid)
 import Network.Snmp.Client.Internal
 import Network.Snmp.Client.Types hiding (timeout, community, hostname, port)
 
+v1 :: Packet
+v1 = initial Version1
+
 v2 :: Packet
 v2 = initial Version2
 
-v1 :: Packet
-v1 = initial Version1
+
+receiveAndFilterRid :: Socket -> RequestId -> IO Packet
+receiveAndFilterRid socket rid = do
+    packet <- decode <$> recv socket 1500 :: IO Packet
+    if rid == getRid packet
+       then return packet
+       else receiveAndFilterRid socket rid
 
 processRequest :: IORef Int32 -> Socket -> (Int32 -> Packet) -> Int -> IO Suite
 processRequest ioref socket requestMakeFunc timeout = do
     rid <- succCounter ioref
     sendAll socket $ encode $ requestMakeFunc rid
-    returnResult2 socket timeout
-
-returnResult2 :: NS.Socket -> Int -> IO Suite
-returnResult2 socket timeout = do
-    result <- race (threadDelay timeout) (decode <$> recv socket 1500 :: IO Packet)
+    result <- race (threadDelay timeout) (receiveAndFilterRid socket rid)
     case result of
          Right packet -> do
              when (getErrorStatus packet /= 0) $ throwIO $ ServerException $ getErrorStatus packet
              return $ getSuite packet
-         Left _ -> throwIO TimeoutException            
+         Left _ -> throwIO TimeoutException
 
 setRCS :: Community -> OIDS -> Packet -> Packet
 setRCS c o = setCommunityP c . setSuite (Suite $ map (`Coupla` Zero) o)
 
-clientV1 :: Hostname -> Port -> Int -> Community -> IO Client
+clientV1 :: Hostname -> Port -> Int -> Int -> Community -> IO Client
 clientV1 = clientV12 v1
 
-clientV2 :: Hostname -> Port -> Int -> Community -> IO Client
+clientV2 :: Hostname -> Port -> Int -> Int -> Community -> IO Client
 clientV2 = clientV12 v2
 
-clientV12 :: Packet -> Hostname -> Port -> Int -> Community -> IO Client
-clientV12 packet hostname port timeout community = do
-    socket <- makeSocket hostname port 
+clientV12 :: Packet -> Hostname -> Port -> Int -> Int -> Community -> IO Client
+clientV12 packet hostname port timeout retryCount community = do
+    socket <- makeSocket hostname port
     uniqInteger <- uniqID
     ref <- newIORef uniqInteger
     let
@@ -64,28 +68,28 @@ clientV12 packet hostname port timeout community = do
                 version = getVersion packet'
                 makeRequest rid = setRequest (GetRequest rid 0 0) packet'
             case version of
-                 Version2 -> processRequest' (makeRequest) timeout
-                 Version1 -> catch (processRequest' (makeRequest) timeout) (fixErrorV1Get oids)
+                 Version2 -> processRequest' makeRequest timeout
+                 Version1 -> catch (processRequest' makeRequest timeout) (fixErrorV1Get oids)
                  Version3 -> error "imposible"
         fixErrorV1Get oids (ServerException 2) = return $ Suite $ map (`Coupla` NoSuchObject) oids
         fixErrorV1Get _ e = throwIO e
 
         bulkget' oids = withSocketsDo $ do
             let makeRequest rid = setRequest (GetBulk rid 0 10) (req oids)
-            processRequest' (makeRequest) timeout
+            processRequest' makeRequest timeout
 
         getnext' oids = withSocketsDo $ do
             let packet' = req oids
                 version = getVersion packet'
                 makeRequest rid = setRequest (GetNextRequest rid 0 0) packet'
             case version of
-                 Version2 -> processRequest' (makeRequest) timeout
-                 Version1 -> catch (processRequest' (makeRequest) timeout) (fixErrorV1GetNext oids)
+                 Version2 -> processRequest' makeRequest timeout
+                 Version1 -> catch (processRequest' makeRequest timeout) (fixErrorV1GetNext oids)
                  Version3 -> error "imposible"
         fixErrorV1GetNext oids (ServerException 2) = return $ Suite $ map (`Coupla` EndOfMibView) oids
         fixErrorV1GetNext _ e = throwIO e
 
-        walk' oids base accumulator 
+        walk' oids base accumulator
             | oids == base = do
                 first <- get' [oids]
                 next <- getnext' [oids]
@@ -103,7 +107,7 @@ clientV12 packet hostname port timeout community = do
                      (_, NoSuchObject) -> walk' next base accumulator
                      (_, NoSuchInstance) -> walk' next base accumulator
                      (_, EndOfMibView) -> return accumulator
-                     (_, _) -> walk' next base (accumulator <> nextData) 
+                     (_, _) -> walk' next base (accumulator <> nextData)
 
         bulkwalk' oids base accumulator = do
                first <- bulkget' [oids]
@@ -115,15 +119,15 @@ clientV12 packet hostname port timeout community = do
                     (True, _) -> return $ accumulator <> filtered first
         set' oids = withSocketsDo $ do
             let makeRequest rid = setRequest (SetRequest rid 0 0) . setCommunityP community . setSuite oids $ packet
-            processRequest' (makeRequest) timeout
+            processRequest' makeRequest timeout
 
-    return Client 
+    return Client
         { get = get'
         , bulkget = bulkget'
         , getnext = getnext'
         , walk = \oids -> mconcat <$> mapM (\oi -> walk' oi oi mempty) oids
         , bulkwalk = \oids -> mconcat <$> mapM (\oi -> bulkwalk' oi oi mempty) oids
-        , set = set' 
+        , set = set'
         , close = NS.close socket
-        } 
+        }
 
