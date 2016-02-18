8bfd684b80300136c259db07fbe4a322563f5581
Anton Kondrachkov <SimpleX91@gmail.com>
2016-02-18 19:54:56 +0300

    Remove HasV2 and HasV3 typeclasses

--- src/Network/Protocol/Snmp.hs
+++ src/Network/Protocol/Snmp.hs
@@ -5,105 +5,133 @@
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE RecordWildCards            #-}
 {-# LANGUAGE StandaloneDeriving         #-}
-module Network.Protocol.Snmp (
--- * snmp types
-  Value(..)
--- * top level types
-, V1
-, V2
-, V3
-, Version(..)
-, Packet
--- ** header
-, Header
--- *** header snmpV2
-, Community(..)
--- *** header snmpV3
-, ID(..)
-, MaxSize(..)
-, Flag(..)
-, SecurityModel(..)
-, SecurityParameter(..)
-, Reportable
-, PrivAuth(..)
-, EngineBootId
-, PrivacyParameter
-, EngineTime
-, EngineId
--- ** PDU
-, PDU (CryptedPDU)
--- *** PDU universal
-, Request(..)
-, RequestId
-, ErrorStatus
-, ErrorIndex
-, Suite(..)
-, Coupla(..)
--- *** PDU snmpV3
-, ContextEngineID(..)
-, ContextName(..)
--- * some classes and helpers
--- *** universal, for work with both versions
-, HasItem(..)
--- *** v2 only, for work with Header V2
-, HasV2(..)
--- *** v3 only, for work with Header V3, PDU V3
-, HasV3(..)
--- *** create new Packet
-, Construct(..)
--- ** helpers for work with Packet
--- *** universal
-, getVersion
-, getRequest
-, setRequest
-, getRid
-, setRid
-, getSuite
-, setSuite
-, getErrorStatus
-, setErrorStatus
--- *** v2 only
-, setCommunityP
--- *** v3 only
-, setIDP
-, setMaxSizeP
-, setUserNameP
-, getAuthenticationParametersP
-, setAuthenticationParametersP
-, setReportableP
-, setPrivAuthP
-, getEngineIdP
-, setEngineIdP
-, getEngineBootsP
-, setEngineBootsP
-, getEngineTimeP
-, setEngineTimeP
-, getPrivParametersP
-, setPrivParametersP
--- * authentication
-, passwordToKey
-, signPacket
-, AuthType(..)
-, PrivType(..)
-, Password
-, Key
-, cleanPass
--- * priv
-, Salt
-, Raw
-, Encrypted
-, Rand32
-, Rand64
-, desEncrypt
-, desDecrypt
-, aesEncrypt
-, aesDecrypt
-, toSalt
--- * exceptions
-, SnmpException(..)
--- * usage example
--- $example
-) where
+module Network.Protocol.Snmp
+    (
+    -- * snmp types
+      Value(..)
+    -- * top level types
+    , V1
+    , V2
+    , V3
+    , Version(..)
+    , Packet
+    -- ** header
+    , Header
+    -- *** header snmpV2
+    , Community(..)
+    -- *** header snmpV3
+    , ID(..)
+    , MaxSize(..)
+    , Flag(..)
+    , SecurityModel(..)
+    , SecurityParameter(..)
+    , Reportable
+    , PrivAuth(..)
+    , EngineBootId
+    , PrivacyParameter
+    , EngineTime
+    , EngineId
+    -- ** PDU
+    , PDU (CryptedPDU)
+    -- *** PDU universal
+    , Request(..)
+    , RequestId
+    , ErrorStatus
+    , ErrorIndex
+    , Suite(..)
+    , Coupla(..)
+    -- *** PDU snmpV3
+    , ContextEngineID(..)
+    , ContextName(..)
+    -- * some classes and helpers
+    -- *** universal, for work with both versions
+    , HasItem(..)
+    -- *** v2 only, for work with Header V2
+    , getCommunity
+    , setCommunity
+    -- *** v3 only, for work with Header V3, PDU V3
+    , getID
+    , getMaxSize
+    , getFlag
+    , getSecurityModel
+    , getSecurityParameter
+    , getAuthoritiveEngineId
+    , getAuthoritiveEngineBoots
+    , getAuthoritiveEngineTime
+    , getUserName
+    , getAuthenticationParameters
+    , getPrivacyParameters
+    , getContextEngineID
+    , getContextName
+    , setID
+    , setMaxSize
+    , setFlag
+    , setSecurityModel
+    , setSecurityParameter
+    , setAuthoritiveEngineId
+    , setAuthoritiveEngineBoots
+    , setAuthoritiveEngineTime
+    , setUserName
+    , setAuthenticationParameters
+    , setPrivacyParameters
+    , setContextEngineID
+    , setContextName
+    -- *** create new Packet
+    , Construct(..)
+    -- ** helpers for work with Packet
+    -- *** universal
+    , getVersion
+    , getRequest
+    , setRequest
+    , getRid
+    , setRid
+    , getSuite
+    , setSuite
+    , getErrorStatus
+    , setErrorStatus
+    -- *** v2 only
+    , setCommunityP
+    -- *** v3 only
+    , setIDP
+    , setMaxSizeP
+    , setUserNameP
+    , getAuthenticationParametersP
+    , setAuthenticationParametersP
+    , setReportableP
+    , setPrivAuthP
+    , getEngineIdP
+    , setEngineIdP
+    , getEngineBootsP
+    , setEngineBootsP
+    , getEngineTimeP
+    , setEngineTimeP
+    , getPrivParametersP
+    , setPrivParametersP
+    -- * authentication
+    , passwordToKey
+    , signPacket
+    , AuthType(..)
+    , PrivType(..)
+    , Password
+    , Key
+    , cleanPass
+    -- * priv
+    , Salt
+    , Raw
+    , Encrypted
+    , Rand32
+    , Rand64
+    , desEncrypt
+    , desDecrypt
+    , aesEncrypt
+    , aesDecrypt
+    , toSalt
+    -- * exceptions
+    , SnmpException(..)
+    -- * usage example
+    -- $example
+    ) where
+
 import           Control.Exception    (Exception, throw)
 import           Control.Monad
 import qualified Crypto.Cipher.AES    as Priv
@@ -324,40 +352,6 @@ class HasItem a where
     getPDU :: Packet -> PDU a
     setPDU :: PDU a -> Packet -> Packet
 
--- | (snmp2 only) getters, setters for work with Header V2
-class HasV2 a where
-    getCommunity :: Header a -> Community
-    setCommunity :: Community -> Header a -> Header a
-
--- | (snmp3 only) getters, setters for work with Header V3 and PDU V3.
-class HasV3 a where
-    getID :: Header a -> ID
-    getMaxSize :: Header a -> MaxSize
-    getFlag :: Header a -> Flag
-    getSecurityModel :: Header a -> SecurityModel
-    getSecurityParameter :: Header a -> SecurityParameter
-    getAuthoritiveEngineId :: Header a -> ByteString
-    getAuthoritiveEngineBoots :: Header a -> Int32
-    getAuthoritiveEngineTime :: Header a -> Int32
-    getUserName :: Header a -> ByteString
-    getAuthenticationParameters :: Header a -> ByteString
-    getPrivacyParameters :: Header a -> ByteString
-    getContextEngineID :: PDU a -> ContextEngineID
-    getContextName :: PDU a -> ContextName
-    setID :: ID -> Header a -> Header a
-    setMaxSize :: MaxSize -> Header a -> Header a
-    setFlag :: Flag -> Header a -> Header a
-    setSecurityModel :: SecurityModel -> Header a -> Header a
-    setSecurityParameter :: SecurityParameter -> Header a -> Header a
-    setAuthoritiveEngineId :: ByteString -> Header a -> Header a
-    setAuthoritiveEngineBoots :: Int32 -> Header a -> Header a
-    setAuthoritiveEngineTime :: Int32 -> Header a -> Header a
-    setUserName :: ByteString -> Header a -> Header a
-    setAuthenticationParameters :: ByteString -> Header a -> Header a
-    setPrivacyParameters :: ByteString -> Header a -> Header a
-    setContextEngineID :: ContextEngineID -> PDU a -> PDU a
-    setContextName :: ContextName -> PDU a -> PDU a
-
 -- | initial new object, like mempty for monoid
 class Construct a where
     initial :: a
@@ -388,6 +382,7 @@ instance Construct Suite where
 
 instance Construct Request where
      initial = GetRequest (RequestId 0) (ErrorStatus 0) (ErrorIndex 0)
+
 ----------------------------------------------------------------------------------------
 instance HasItem V2 where
     getHeader (V2Packet _ x _) = x
@@ -409,41 +404,98 @@ instance HasItem V3 where
     setPDU p (V3Packet v h _) = V3Packet v h p
     setPDU _ _ = undefined
 
-instance HasV2 V2 where
-    getCommunity (V2Header c) = c
-    setCommunity c (V2Header _) = V2Header c
+getCommunity :: Header V2 -> Community
+getCommunity (V2Header c) = c
 
-instance HasV3 V3 where
-    getID (V3Header i _ _ _ _) = i
-    getMaxSize (V3Header _ i _ _ _) = i
-    getFlag (V3Header _ _ i _ _) = i
-    getSecurityModel (V3Header _ _ _ i _) = i
-    getSecurityParameter (V3Header _ _ _ _ i) = i
-    getAuthoritiveEngineId = authoritiveEngineId . getSecurityParameter
-    getAuthoritiveEngineBoots = authoritiveEngineBoots . getSecurityParameter
-    getAuthoritiveEngineTime = authoritiveEngineTime . getSecurityParameter
-    getUserName = userName . getSecurityParameter
-    getAuthenticationParameters = authenticationParameters . getSecurityParameter
-    getPrivacyParameters = privacyParameters . getSecurityParameter
-    getContextEngineID (ScopedPDU i _ _) = i
-    getContextEngineID _ = undefined
-    getContextName (ScopedPDU _ i _) = i
-    getContextName _ = undefined
-    setID i (V3Header _ a b c d) = V3Header i a b c d
-    setMaxSize i (V3Header a _ b c d) = V3Header a i b c d
-    setFlag i (V3Header a b _ c d) = V3Header a b i c d
-    setSecurityModel i (V3Header a b c _ d) = V3Header a b c i d
-    setSecurityParameter i (V3Header a b c d _) = V3Header a b c d i
-    setAuthoritiveEngineId      i (V3Header a b c d f) = V3Header a b c d (f { authoritiveEngineId = i })
-    setAuthoritiveEngineBoots   i (V3Header a b c d f) = V3Header a b c d (f { authoritiveEngineBoots = i })
-    setAuthoritiveEngineTime    i (V3Header a b c d f) = V3Header a b c d (f { authoritiveEngineTime = i })
-    setUserName                 i (V3Header a b c d f) = V3Header a b c d (f { userName = i })
-    setAuthenticationParameters i (V3Header a b c d f) = V3Header a b c d (f { authenticationParameters = i })
-    setPrivacyParameters        i (V3Header a b c d f) = V3Header a b c d (f { privacyParameters = i })
-    setContextEngineID i (ScopedPDU _ b c) = ScopedPDU i b c
-    setContextEngineID _ _ = undefined
-    setContextName i (ScopedPDU a _ b) = ScopedPDU a i b
-    setContextName _ _ = undefined
+setCommunity :: Community -> Header V2 -> Header V2
+setCommunity c (V2Header _) = V2Header c
+
+getID :: Header V3 -> ID
+getID (V3Header i _ _ _ _) = i
+
+getMaxSize :: Header V3 -> MaxSize
+getMaxSize (V3Header _ i _ _ _) = i
+
+getFlag :: Header V3 -> Flag
+getFlag (V3Header _ _ i _ _) = i
+
+getSecurityModel :: Header V3 -> SecurityModel
+getSecurityModel (V3Header _ _ _ i _) = i
+
+getSecurityParameter :: Header V3 -> SecurityParameter
+getSecurityParameter (V3Header _ _ _ _ i) = i
+
+getAuthoritiveEngineId :: Header V3 -> ByteString
+getAuthoritiveEngineId = authoritiveEngineId . getSecurityParameter
+
+getAuthoritiveEngineBoots :: Header V3 -> Int32
+getAuthoritiveEngineBoots = authoritiveEngineBoots . getSecurityParameter
+
+getAuthoritiveEngineTime :: Header V3 -> Int32
+getAuthoritiveEngineTime = authoritiveEngineTime . getSecurityParameter
+
+getUserName :: Header V3 -> ByteString
+getUserName = userName . getSecurityParameter
+
+getAuthenticationParameters :: Header V3 -> ByteString
+getAuthenticationParameters = authenticationParameters . getSecurityParameter
+
+getPrivacyParameters :: Header V3 -> ByteString
+getPrivacyParameters = privacyParameters . getSecurityParameter
+
+getContextEngineID :: PDU V3 -> ContextEngineID
+getContextEngineID (ScopedPDU i _ _) = i
+getContextEngineID _ = undefined
+
+getContextName :: PDU V3 -> ContextName
+getContextName (ScopedPDU _ i _) = i
+getContextName _ = undefined
+
+setID :: ID -> Header V3 -> Header V3
+setID i (V3Header _ a b c d) = V3Header i a b c d
+
+setMaxSize :: MaxSize -> Header V3 -> Header V3
+setMaxSize i (V3Header a _ b c d) = V3Header a i b c d
+
+setFlag :: Flag -> Header V3 -> Header V3
+setFlag i (V3Header a b _ c d) = V3Header a b i c d
+
+setSecurityModel :: SecurityModel -> Header V3 -> Header V3
+setSecurityModel i (V3Header a b c _ d) = V3Header a b c i d
+
+setSecurityParameter :: SecurityParameter -> Header V3 -> Header V3
+setSecurityParameter i (V3Header a b c d _) = V3Header a b c d i
+
+setAuthoritiveEngineId :: ByteString -> Header V3 -> Header V3
+setAuthoritiveEngineId i (V3Header a b c d f) =
+    V3Header a b c d (f { authoritiveEngineId = i })
+
+setAuthoritiveEngineBoots :: Int32 -> Header V3 -> Header V3
+setAuthoritiveEngineBoots i (V3Header a b c d f) =
+    V3Header a b c d (f { authoritiveEngineBoots = i })
+
+setAuthoritiveEngineTime :: Int32 -> Header V3 -> Header V3
+setAuthoritiveEngineTime i (V3Header a b c d f) =
+    V3Header a b c d (f { authoritiveEngineTime = i })
+
+setUserName :: ByteString -> Header V3 -> Header V3
+setUserName i (V3Header a b c d f) = V3Header a b c d (f { userName = i })
+
+setAuthenticationParameters :: ByteString -> Header V3 -> Header V3
+setAuthenticationParameters i (V3Header a b c d f) =
+    V3Header a b c d (f { authenticationParameters = i })
+
+setPrivacyParameters :: ByteString -> Header V3 -> Header V3
+setPrivacyParameters i (V3Header a b c d f) =
+    V3Header a b c d (f { privacyParameters = i })
+
+setContextEngineID :: ContextEngineID -> PDU V3 -> PDU V3
+setContextEngineID i (ScopedPDU _ b c) = ScopedPDU i b c
+setContextEngineID _ _ = undefined
+
+setContextName :: ContextName -> PDU V3 -> PDU V3
+setContextName i (ScopedPDU a _ b) = ScopedPDU a i b
+setContextName _ _ = undefined
 
 ----------------------------------------------------------------------------------------
 setIDP :: ID -> Packet -> Packet
