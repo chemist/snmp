fd290b28a108e285d4ac37ee2172361809a8f155
Anton Kondrachkov <SimpleX91@gmail.com>
2016-02-18 18:11:45 +0300

    Add pack benchmark

--- benchmarks/Main.hs
+++ benchmarks/Main.hs
@@ -1,8 +1,9 @@
 module Main where
 
-import Criterion.Main
-import Network.Protocol.Snmp
-import Data.Serialize 
+import           Criterion.Main
+import           Data.ByteString       (ByteString)
+import           Data.Serialize
+import           Network.Protocol.Snmp
 
 
 main :: IO ()
@@ -11,8 +12,14 @@ main = defaultMain
     [ bench "pack unpack version 1" $ whnf packUnpack Version1
     , bench "pack unpack version 2" $ whnf packUnpack Version2
     , bench "pack unpack version 3" $ whnf packUnpack Version3
+    , bench "pack version 1" $ whnf pack Version1
+    , bench "pack version 2" $ whnf pack Version2
+    , bench "pack version 3" $ whnf pack Version3
     ]
   ]
+
+pack :: Version -> ByteString
+pack v = encode (initial v :: Packet)
 
 packUnpack :: Version -> Either String Packet
 packUnpack v = decode (encode (initial v :: Packet))
