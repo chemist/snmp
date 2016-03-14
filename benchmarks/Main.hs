cc215aa62552913702c3bd737c12d3fdeabd8855
Anton Kondrachkov <SimpleX91@gmail.com>
2016-03-14 19:48:48 +0300

    Add passwordToKey benchmark

--- benchmarks/Main.hs
+++ benchmarks/Main.hs
@@ -7,14 +7,18 @@ import           Network.Protocol.Snmp
 main :: IO ()
 main = defaultMain
   [ bgroup "packet pack/unpack"
-    [ bench "pack unpack version 1" $ whnf packUnpackPacket Version1
-    , bench "pack unpack version 2" $ whnf packUnpackPacket Version2
-    , bench "pack unpack version 3" $ whnf packUnpackPacket Version3
+    [ bench "version 1" $ whnf packUnpackPacket Version1
+    , bench "version 2" $ whnf packUnpackPacket Version2
+    , bench "version 3" $ whnf packUnpackPacket Version3
     ]
   , bgroup "value pack/unpack"
-    [ bench "pack integer" $ whnf packUnpackValue (Integer 65535)
-    , bench "pack long oid" $ whnf packUnpackValue (OI $ Oid [1..65535])
-    , bench "pack OctetString" $ whnf packUnpackValue (OctetString "some test string")
+    [ bench "integer" $ whnf packUnpackValue (Integer 65535)
+    , bench "long oid" $ whnf packUnpackValue (OI $ Oid [1..65535])
+    , bench "OctetString" $ whnf packUnpackValue (OctetString "some test string")
+    ]
+  , bgroup "encryption: password to key"
+    [ bench "MD5" $ whnf pToK MD5
+    , bench "SHA" $ whnf pToK SHA
     ]
   ]
 
@@ -23,4 +27,7 @@ packUnpackPacket v = decode (encode (initial v :: Packet))
 
 packUnpackValue :: Value -> Either String Value
 packUnpackValue = decode . encode
+
+pToK :: AuthType -> Key
+pToK at = passwordToKey at (Password "mytestpassword") (EngineID "abcdefghi")
 
